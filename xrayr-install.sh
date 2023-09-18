4c4301295af60de9ef5c9732aed21c7d1dc4a40e
NickyRomeo <56181327+Nickyromeo@users.noreply.github.com>
2023-09-19 03:11:18 +0800

    Update xrayr-install.sh

--- xrayr-install.sh
+++ xrayr-install.sh
@@ -140,7 +140,7 @@ install_XrayR() {
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
     rm /etc/systemd/system/XrayR.service -f
-    file="https://github.littlejerry.tk/https://github.com/XrayR-project/XrayR-release/raw/master/XrayR.service"
+    file="https://github.littlejerry.tk/https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.service"
     wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
     #cp -f XrayR.service /etc/systemd/system/
     systemctl daemon-reload
